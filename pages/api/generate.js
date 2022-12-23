91ca75af98437e66aef0d2e88d00f535a078548f
Jeffrey Zang <zangj4548@wrdsb.ca>
2022-12-23 14:25:02 -0500

    add among us

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -7,7 +7,20 @@ const configuration = new Configuration({
 });
 const openai = new OpenAIApi(configuration);
 
+const amonguses = [
+  "A game mode where players can customize their own character with different skins, hats, and outfits.",
+  "A tool that allows players to design their own Among Us maps, with custom tasks and layouts.",
+  "Design a virtual reality version of Among Us that allows players to fully immerse themselves in the game.",
+  "Build a platform that allows players to host and organize their own Among Us tournaments, with support for custom rules and brackets.",
+  "Develop a tool that analyzes gameplay data from Among Us matches and generates insights and recommendations for improvement.",
+  "Create an educational version of Among Us that teaches players about teamwork, communication, and problem-solving through gameplay.",
+  "You sus"
+]
+
 export default async function (req, res) {
+  if (req.body.theme == 'among us') {
+    res.status(200).json({ result: amonguses[Math.floor(Math.random()*amonguses.length)] });
+  }
   const completion = await openai.createCompletion({
     model: "text-curie-001",
     prompt: generatePrompt(req.body.theme),
